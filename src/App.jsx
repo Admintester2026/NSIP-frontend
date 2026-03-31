a09ee6404b750159501099ee94e5217f9c4d8760
Admintester2026 <TesterNSIP2026@outlook.es>
2026-03-31 12:06:43 -0600

    fix: Actualizar rutas de importación a Mantenimiento con mayúscula

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+﻿import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Modulos from './pages/Modulos';
 
-//Módulo planta
+//MÃ³dulo planta
 import PlantasDashboard from './pages/modules/Plantas/Dashboard';
 import PlantasCycles from './pages/modules/Plantas/Cycles';
 import PlantasStats from './pages/modules/Plantas/Stats';
 import PlantasComparativa from './pages/modules/Plantas/Comparativa';
 
-//Módulo voltaje
+//MÃ³dulo voltaje
 import VoltajeGraficas from './components/Sensorvoltaje/VoltajeGraficas';
 import VoltajeDashboard from './pages/modules/Voltaje/VoltajeDashboard';
 import VoltajeStats from './pages/modules/Voltaje/VoltajeStats';
@@ -20,10 +20,10 @@ import { ModeProvider } from './context/ModeContext';
 import './styles/global.css';
 
 // Sistema de Mantenimiento.
-import Equipos from './pages/mantenimiento/Equipos';
-import Ordenes from './pages/mantenimiento/Ordenes';
-import Papelera from './pages/mantenimiento/Papelera';
-import DetalleEquipo from './pages/mantenimiento/DetalleEquipo';
+import Equipos from './pages/Mantenimiento/Equipos';
+import Ordenes from './pages/Mantenimiento/Ordenes';
+import Papelera from './pages/Mantenimiento/Papelera';
+import DetalleEquipo from './pages/Mantenimiento/DetalleEquipo';
 
 function App() {
   return (
@@ -31,19 +31,19 @@ function App() {
       <ModeProvider>
         <Navbar />
         <Routes>
-          {/* Página principal */}
+          {/* PÃ¡gina principal */}
           <Route path="/" element={<Home />} />
 
-          {/* Página de módulos */}
+          {/* PÃ¡gina de mÃ³dulos */}
           <Route path="/modulos" element={<Modulos />} />
 
-          {/* Módulo de Luminarias */}
+          {/* MÃ³dulo de Luminarias */}
           <Route path="/modulos/luminarias" element={<PlantasDashboard />} />
           <Route path="/modulos/luminarias/ciclos" element={<PlantasCycles />} />
           <Route path="/modulos/luminarias/estadisticas" element={<PlantasStats />} />
           <Route path="/modulos/luminarias/comparativa" element={<PlantasComparativa />} />
 
-          {/* Módulo de voltaje */}
+          {/* MÃ³dulo de voltaje */}
           <Route path="/modulos/voltaje" element={<VoltajeDashboard />} />
           <Route path="/modulos/voltaje/estadisticas" element={<VoltajeStats />} />
           <Route path="/modulos/voltaje/historico" element={<VoltajeHistorico />} />
@@ -70,3 +70,4 @@ function App() {
 }
 
 export default App;
+
